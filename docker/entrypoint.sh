8b21ea986b9817b403d9029a439e8a91a65d6e15
Julio C <202300081@uprr.edu.mx>
2026-04-23 21:11:26 -0400

    fix: update entrypoint script to configure MPM for Apache runtime

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -15,9 +15,10 @@ if [ "${RUN_MIGRATIONS}" = "true" ]; then
   php artisan migrate --force
 fi
 
-# Fix para Railway: usar el puerto dinámico
-PORT=${PORT:-80}
-sed -i "s/Listen 80/Listen $PORT/" /etc/apache2/ports.conf
-sed -i "s/*:80/*:$PORT/" /etc/apache2/sites-available/000-default.conf
+# Fix MPM en runtime
+rm -f /etc/apache2/mods-enabled/mpm_*.load \
+      /etc/apache2/mods-enabled/mpm_*.conf
+ln -sf /etc/apache2/mods-available/mpm_prefork.load /etc/apache2/mods-enabled/mpm_prefork.load
+ln -sf /etc/apache2/mods-available/mpm_prefork.conf /etc/apache2/mods-enabled/mpm_prefork.conf
 
 exec "$@"
